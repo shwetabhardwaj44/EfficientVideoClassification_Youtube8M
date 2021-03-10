e3e19e029520148fbdd535802e33faef5a6ffaef
Shweta Bhardwaj <shweta.bhardwaj@Shwetas-MacBook-Pro.local>
2021-03-10 14:20:57 +0530

    clean_code

--- run_finetune.sh
+++ run_finetune.sh
@@ -2,7 +2,7 @@
 # FINETUNING STUDENT
 
 #epc1
-time CUDA_VISIBLE_DEVICES="0"  python -u code_student_uniform/code/train_finetune.py --train_data_pattern "./yt8m/train*.tfrecord" --train_dir  ./model_HLSTM_TeaStud_every10_finetune/   --frame_features True     --feature_names  "rgb, audio" --feature_sizes "1024, 128"   --model "HierarchicalLstmModel" --gpu 6    --batch_size 256  --num_inputs_to_lstm 20 --lstm_layers 2 --start_new_model False  --num_epochs 1  --every_n 10  &> output_HLSTM_TeaStud_every10_finetune_after_1epc
+time CUDA_VISIBLE_DEVICES="0"  python -u code_student_uniform/train_finetune.py --train_data_pattern "./yt8m/train*.tfrecord" --train_dir  ./model_HLSTM_TeaStud_every10_finetune/   --frame_features True     --feature_names  "rgb, audio" --feature_sizes "1024, 128"   --model "HierarchicalLstmModel" --gpu 6    --batch_size 256  --num_inputs_to_lstm 20 --lstm_layers 2 --start_new_model False  --num_epochs 1  --every_n 10  &> output_HLSTM_TeaStud_every10_finetune_after_1epc
 
 # Saving checkpoint after every epoch of Fine-tuning !
 mkdir ./model_HLSTM_TeaStud_every10_finetune/backup_epc1
@@ -12,7 +12,7 @@ cp -r ./model_HLSTM_TeaStud_every10_finetune/model.ckpt* ./model_HLSTM_TeaStud_e
 cp ./model_HLSTM_TeaStud_every10_finetune/check* ./model_HLSTM_TeaStud_every10_finetune/backup_epc1/
 
 #epc2
-time CUDA_VISIBLE_DEVICES="0"  python -u code_student_uniform/code/train_finetune.py --train_data_pattern "./yt8m/train*.tfrecord" --train_dir  ./model_HLSTM_TeaStud_every10_finetune/   --frame_features True     --feature_names  "rgb, audio" --feature_sizes "1024, 128"   --model "HierarchicalLstmModel" --gpu 6    --batch_size 256  --num_inputs_to_lstm 20 --lstm_layers 2 --start_new_model False  --num_epochs 1  --every_n 10  &> output_HLSTM_TeaStud_every10_finetune_after_2epc
+time CUDA_VISIBLE_DEVICES="0"  python -u code_student_uniform/train_finetune.py --train_data_pattern "./yt8m/train*.tfrecord" --train_dir  ./model_HLSTM_TeaStud_every10_finetune/   --frame_features True     --feature_names  "rgb, audio" --feature_sizes "1024, 128"   --model "HierarchicalLstmModel" --gpu 6    --batch_size 256  --num_inputs_to_lstm 20 --lstm_layers 2 --start_new_model False  --num_epochs 1  --every_n 10  &> output_HLSTM_TeaStud_every10_finetune_after_2epc
 
 mkdir ./model_HLSTM_TeaStud_every10_finetune/backup_epc2
 mkdir ./model_HLSTM_TeaStud_every10_finetune/backup_epc2/train
@@ -21,7 +21,7 @@ cp -r ./model_HLSTM_TeaStud_every10_finetune/model.ckpt* ./model_HLSTM_TeaStud_e
 cp ./model_HLSTM_TeaStud_every10_finetune/check* ./model_HLSTM_TeaStud_every10_finetune/backup_epc2/
 
 #epc3
-time CUDA_VISIBLE_DEVICES="0"  python -u code_student_uniform/code/train_finetune.py --train_data_pattern "./yt8m/train*.tfrecord" --train_dir  ./model_HLSTM_TeaStud_every10_finetune/   --frame_features True     --feature_names  "rgb, audio" --feature_sizes "1024, 128"   --model "HierarchicalLstmModel" --gpu 6    --batch_size 256  --num_inputs_to_lstm 20 --lstm_layers 2 --start_new_model False  --num_epochs 1  --every_n 10  &> output_HLSTM_TeaStud_every10_finetune_after_3epc
+time CUDA_VISIBLE_DEVICES="0"  python -u code_student_uniform/train_finetune.py --train_data_pattern "./yt8m/train*.tfrecord" --train_dir  ./model_HLSTM_TeaStud_every10_finetune/   --frame_features True     --feature_names  "rgb, audio" --feature_sizes "1024, 128"   --model "HierarchicalLstmModel" --gpu 6    --batch_size 256  --num_inputs_to_lstm 20 --lstm_layers 2 --start_new_model False  --num_epochs 1  --every_n 10  &> output_HLSTM_TeaStud_every10_finetune_after_3epc
 
 mkdir ./model_HLSTM_TeaStud_every10_finetune/backup_epc3
 mkdir ./model_HLSTM_TeaStud_every10_finetune/backup_epc3/train
