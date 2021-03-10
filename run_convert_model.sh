227513a1a3ebb239657ab89cca0fa89f7e82642b
Shweta Bhardwaj <shweta.bhardwaj@Shwetas-MacBook-Pro.local>
2021-03-10 11:51:46 +0530

    edited scripts

--- run_convert_model.sh
+++ run_convert_model.sh
@@ -1,3 +1,3 @@
 
 
-time CUDA_VISIBLE_DEVICES="1"  python -u code_student_uniform/code/train_convert_model.py --train_data_pattern "./yt8m/train*.tfrecord" --train_dir  ./model_HLSTM_TeaStud_every10_train/   --frame_features True     --feature_names  "rgb, audio" --feature_sizes "1024, 128"   --model "HierarchicalLstmModel" --gpu 6    --batch_size 128  --num_inputs_to_lstm 20 --lstm_layers 2 --start_new_model True  --num_epochs 1  --every_n 10  &> output_HLSTM_TeaStud_every10_convertModel
+time CUDA_VISIBLE_DEVICES="0"  python -u code_student_uniform/code/train_convert_model.py --train_data_pattern "./yt8m/train*.tfrecord" --train_dir  ./model_HLSTM_TeaStud_every10_train/   --frame_features True     --feature_names  "rgb, audio" --feature_sizes "1024, 128"   --model "HierarchicalLstmModel" --gpu 6    --batch_size 128  --num_inputs_to_lstm 20 --lstm_layers 2 --start_new_model True  --num_epochs 1  --every_n 10  &> output_HLSTM_TeaStud_every10_convertModel
--- run_validate.sh
+++ run_validate.sh
@@ -1,8 +1,8 @@
 
 # Evaluate after epoch 1 of Training:
 
-time CUDA_VISIBLE_DEVICES="0"  python -u code_student_uniform/code/validate.py --eval_data_pattern "./yt8m/validate*.tfrecord" --train_dir  ./model_HLSTM_TeaStud_every10_train/backup_epc1/   --frame_features True     --feature_names  "rgb, audio" --feature_sizes "1024, 128"   --model "HierarchicalLstmModel" --gpu 1    --batch_size 512  --num_inputs_to_lstm 20 --lstm_layers 2 --start_new_model False  --num_epochs 1  --every_n 10  --top_k 20 --run_once True &> validate_HLSTM_TeaStud_every10_train_after_1epc
+time CUDA_VISIBLE_DEVICES="0"  python -u code_student_uniform/code/validate.py --eval_data_pattern "./yt8m/validate*.tfrecord" --train_dir  ./model_HLSTM_TeaStud_every10_train/backup_epc1/   --frame_features True     --feature_names  "rgb, audio" --feature_sizes "1024, 128"   --model "HierarchicalLstmModel" --gpu 1    --batch_size 512  --num_inputs_to_lstm 20 --lstm_layers 2 --start_new_model False  --num_epochs 1  --every_n 10  --top_k 20 --run_once True &> validate_HLSTM_TeaStud_every10_train_after_5epc
 
-mkdir ./model_HLSTM_TeaStud_every10_train/backup_epc1/evals
-mv ./model_HLSTM_TeaStud_every10_train/backup_epc1/events* ./model_HLSTM_TeaStud_every10_train/backup_epc1/evals/
+mkdir ./model_HLSTM_TeaStud_every10_train/backup_epc5/evals
+mv ./model_HLSTM_TeaStud_every10_train/backup_epc5/events* ./model_HLSTM_TeaStud_every10_train/backup_epc5/evals/
 
